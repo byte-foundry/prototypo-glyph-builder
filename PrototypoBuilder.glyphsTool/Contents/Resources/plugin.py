582dc7cc63ac7739d85ab04a89005a27a73681c8
FranzPoize <francois.poizat@gmail.com>
2018-06-26 18:19:18 +0200

    filled in plugin template

---
+++ PrototypoBuilder.glyphsTool/Contents/Resources/plugin.py
@@ -0,0 +1,96 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Select Tool Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/SelectTool
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class PrototypoBuilder(SelectTool):
+	
+	def settings(self):
+		self.name = Glyphs.localize({'en': u'My Select Tool', 'de': u'Mein Auswahlwerkzeug'})
+		self.generalContextMenus = [
+			{'name': Glyphs.localize({'en': u'Layer info in Macro window', 'de': u'Ebenen-Infos in Makro-Fenster'}), 'action': self.printInfo},
+		]
+		self.keyboardShortcut = 'c'
+
+	def start(self):
+		pass
+
+	def activate(self):
+		pass
+
+	def background(self, layer):
+
+		# Draw a red rectangle behind the glyph as big as the glyph’s bounding box
+		NSColor.redColor().set()
+		NSBezierPath.fillRect_(layer.bounds)
+
+	def deactivate(self):
+		pass
+		
+	def conditionalContextMenus(self):
+
+		# Empty list of context menu items
+		contextMenus = []
+
+		# Execute only if layers are actually selected
+		if Glyphs.font.selectedLayers:
+			layer = Glyphs.font.selectedLayers[0]
+			
+			# Exactly one object is selected and it’s an anchor
+			if len(layer.selection) == 1 and type(layer.selection[0]) == GSAnchor:
+					
+				# Add context menu item
+				contextMenus.append({'name': Glyphs.localize({'en': u'Randomly move anchor', 'de': u'Anker zufällig verschieben'}), 'action': self.randomlyMoveAnchor})
+
+		# Return list of context menu items
+		return contextMenus
+
+	def printInfo(self):
+		"""
+		Example for a method triggered by a context menu item.
+		Fill in your own method name and code.
+		Remove this method if you do not want any extra context menu items.
+		"""
+
+		# Execute only if layers are actually selected
+		if Glyphs.font.selectedLayers:
+			layer = Glyphs.font.selectedLayers[0]
+		
+			# Do stuff:
+			print "Current layer:", layer.parent.name, layer.name
+			print "  Number of paths:", len(layer.paths)
+			print "  Number of components:", len(layer.components)
+			print "  Number of anchors:", len(layer.anchors)
+
+	def randomlyMoveAnchor( self, sender ):
+		"""
+		Example for a method triggered by a conditional context menu item.
+		Fill in your own method name and code.
+		Sender contains the NSMenuItem, so that’s not really useful for you.
+		Remove this method if you do not want any extra context menu items.
+		"""
+		import random
+		
+		# Just for your understanding:
+		# Here we don’t need to check how many objects are selected and what types they are.
+		# Because this has already been done in conditionalContextMenus() and didn’t change since.
+		# So our anchor is the first and only selected item in the layer.
+
+		anchor = Glyphs.font.selectedLayers[0].selection[0]
+		anchor.position = NSPoint(anchor.position.x + random.randint(-50, 50), anchor.position.y + random.randint(-50, 50))
+
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
